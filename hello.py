62802bff2dd964c8dbfd77f0eb707be04aaa4e5d
zhirans2 <zhirans2@illinois.edu>
2019-04-27 23:41:05 -0500

    add

--- hello.py
+++ hello.py
@@ -237,7 +237,6 @@ def geteve():
                 tmp = database.getEvent(eid)[0]
                 print(tmp)
                 event={}
-                iid = int(tmp[0])
                 hostID = int(tmp[1])
                 event["host"] = database.getUsernameByUid(hostID)[0][0]
                 partIDs = tmp[2].split(';')
@@ -251,16 +250,7 @@ def geteve():
                 event["title"] = tmp[5]
                 event["description"] = tmp[6]
                 event["state"] = tmp[7]
-                event["id"] = iid
-
-                accepted = False
-                status = database.findAcceptedEvent(session['uid'], iid)
-                print("status",status)
-                if len(status)>0:
-                    accepted=True
-
-                event["accepted"] = accepted
-
+                event["id"] = int(tmp[0])
                 results.append(event)
         # ids = json.loads(database.findUser("invitations", session['uid'])[0][0])
         # print(ids)
@@ -290,12 +280,7 @@ def joinInv():
     category = ''
     description = event[6]
 
-    status = database.findAcceptedEvent(session['uid'], invId)
-    print("status",status)
-    if len(status)==0:
-        database.acceptEvent(session['uid'], invId, "accepted")
-        database.addActivity(session['uid'], title, start, end, willingness, category, description)
-
+    database.addActivity(session['uid'], title, start, end, willingness, category, description)
     result={"iid":invId}
     return json.dumps(result)
     # database.invAddMember(invId,uid)
